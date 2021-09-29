542f9bb6213dd613c34149b4cd743db985065697
shakil-babu <personal.shakil.babu@gmail.com>
2021-09-29 21:08:51 +0600

    Project initialized

---
+++ dailycodes-client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => { 
+  return (
+    <div>
+        <h1>See you not for mind!</h1>
+        <h1>Have a relax!</h1>
+    </div>
+  )
+}
+
+export default App
